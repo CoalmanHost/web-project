2543a216f77531aa1a35af8543c2e74581603736
Timofei <tim.beliachenko@gmail.com>
2021-03-22 18:25:48 +0300

    Register to react

---
+++ src/RegisterForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./RegisterForm.css";
+
+const Register = () => {
+    return (
+        <div className="register">
+            <h2>Регистрация</h2>
+            <form name="register" method="post" action="" autoComplete="on">
+                <h4>Введите имя</h4>
+                <label>
+                    <input name = "userName" type="text" placeholder="Sith"/>
+                </label>
+                <h4>Введите email</h4>
+                <label>
+                    <input name = "userEmail" type="email" placeholder="ivanovich@gmail.com"/>
+                </label>
+                <h4>Введите пароль</h4>
+                <label>
+                    <input name = "userPassword" className="password-input" type="password" placeholder="123456789"/>
+                        <a href="#" id="password-control"></a>
+                </label>
+                <h4>Введите пароль повторно</h4>
+                <label>
+                    <input className="password-input" type="password" content="Введите пароль повторно"/>
+                </label>
+                <input id="button" type="button" value="Стать джедаем"/>
+            </form>
+        </div>
+);
+}
+export default Register
