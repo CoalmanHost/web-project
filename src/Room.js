a89f3bb097190a9bce60fb7b891bc6dd60bd5f64
Timofei <tim.beliachenko@gmail.com>
2021-05-01 22:06:01 +0300

    some sort of gameplay

--- src/Room.js
+++ src/Room.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import "./Room.css";
 
-function addCard(){
-    document.getElementById("CardPlace").innerHTML += "<div class='card' id='card'></div>"
+
+function move_To(id){
+
 }
 
-document.getElementById("card").addEventListener("click", function (){
-    document.getElementById( "card").onmousedown = moveCard;
-})
-
-function moveCard(){
-    const el = document.getElementById("card");
-    el.style.left = el.offsetLeft + "px";
+function addCard() {
+    const generated = Math.ceil(Math.random() * 1000000);
+    const classGenerator = Math.ceil(Math.random() * 3);
+    document.getElementById("CardPlace").innerHTML += "<div class='card " + classGenerator + "' id ='" + generated + "'>" + Math.ceil(Math.random() * 10) + "</div>"
+    document.getElementById(generated).addEventListener('click', function (){
+        const card = document.getElementById(generated);
+        const classNum = card.classList;
+        let addCardTo;
+        if ("3" === classNum[1]){
+            addCardTo = document.getElementById("bombardier");
+        }
+        if ("2" === classNum[1]){
+            addCardTo = document.getElementById("interceptor");
+        }
+        if ("1" === classNum[1]){
+            addCardTo = document.getElementById("cruiser");
+        }
+        addCardTo.innerHTML += card.outerHTML;
+        document.getElementById("CardPlace").removeChild(card);
+    })
 }
-
 
 const Room = () => {
  return(
@@ -39,13 +52,13 @@ const Room = () => {
                  </div>
              </div>
              <div className={"rows"}>
-                 <div className={"cruiser game-section"}>
+                 <div className={"game-section"} id={"cruiser"}>
 
                  </div>
-                 <div className={"interceptor game-section"}>
+                 <div className={"game-section"} id={"interceptor"}>
 
                  </div>
-                 <div className={"bombardier game-section"}>
+                 <div className={"game-section"} id={"bombardier"}>
 
                  </div>
                  <div className={"myCards"} id={"CardPlace" }>
